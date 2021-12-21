ab23c93ea8db9c7f7d2daf8625ccc336c43e5a9b
Rafael Andrade <devrafael72@gmail.com>
2021-12-21 14:57:08 -0300

    Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,25 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     'import/prefer-default-export': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
+    'consistent-return': 'off',
     'no-console': 'off',
+    quotes: 'off',
   },
 };
