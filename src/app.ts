f36543471bca7765d43ce849474779c785166435
Rafael Andrade <devrafael72@gmail.com>
2021-12-21 14:25:21 -0300

    Segurança

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,30 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { router } from './routes';
 
 export const app = express();
 
 app.use(router);
+app.use(express.json());
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'Content-Type',
+    'Accept',
+    'authorization',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+};
+
+app.use(helmet());
+app.use(cors(options));
+app.options('*', cors(options));
