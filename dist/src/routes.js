419d0cceb0e146153d8af590f932e5eacc238507
Rafael Andrade <devrafael72@gmail.com>
2021-12-21 14:38:27 -0300

    Rota

--- dist/src/routes.js
+++ dist/src/routes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
 exports.router = (0, express_1.Router)();
+exports.router.get('/', function (req, res) { return res.json({ status: 'ativo' }); });
 exports.router.get('/produtos', function (request, response) {
     console.log('chamado');
     var products = [
@@ -10,49 +11,49 @@ exports.router.get('/produtos', function (request, response) {
             name: 'Produto A',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto B',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto C',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto D',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto E',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto F',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto G',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto H',
             price: 150,
             description: 'lorem ipsum dolor sit ammet consectetur',
-            estoque: 12
+            estoque: 12,
         },
         {
             name: 'Produto I',
@@ -68,6 +69,6 @@ exports.router.get('/produtos', function (request, response) {
         },
     ];
     return response.json({
-        products: products
+        products: products,
     });
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,58 +1,60 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-export const router = Router()
+export const router = Router();
+
+router.get('/', (req, res) => res.json({ status: 'ativo' }));
 
 router.get('/produtos', (request, response) => {
   console.log('chamado');
-  
+
   const products = [
     {
       name: 'Produto A',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto B',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto C',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto D',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto E',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto F',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto G',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto H',
       price: 150,
       description: 'lorem ipsum dolor sit ammet consectetur',
-      estoque: 12
+      estoque: 12,
     },
     {
       name: 'Produto I',
@@ -66,8 +68,8 @@ router.get('/produtos', (request, response) => {
       description: 'lorem ipsum dolor sit ammet consectetur',
       estoque: 12,
     },
-  ]
+  ];
   return response.json({
-    products
-  })
-})
+    products,
+  });
+});
