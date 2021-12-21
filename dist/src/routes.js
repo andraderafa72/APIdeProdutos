51e285bd06553d37d969fcda887579186f33870a
Rafael Andrade <devrafael72@gmail.com>
2021-12-21 14:09:55 -0300

    Initial commit

---
+++ dist/src/routes.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+var express_1 = require("express");
+exports.router = (0, express_1.Router)();
+exports.router.get('/produtos', function (request, response) {
+    console.log('chamado');
+    var products = [
+        {
+            name: 'Produto A',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto B',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto C',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto D',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto E',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto F',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto G',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto H',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12
+        },
+        {
+            name: 'Produto I',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12,
+        },
+        {
+            name: 'Produto J',
+            price: 150,
+            description: 'lorem ipsum dolor sit ammet consectetur',
+            estoque: 12,
+        },
+    ];
+    return response.json({
+        products: products
+    });
+});
---
+++ dist/src/server.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_1 = require("./app");
+app_1.app.listen(3334, function () { return console.log('Servidor rodando na porta 3334: https://localhost:3334'); });
