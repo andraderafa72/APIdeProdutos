86a9d34e055ecfc06046edcd0f20908dd7708438
Rafael Andrade <devrafael72@gmail.com>
2021-12-21 14:26:37 -0300

    Build

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,7 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
+/* eslint-disable import/first */
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+require("reflect-metadata");
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var helmet_1 = __importDefault(require("helmet"));
 var routes_1 = require("./routes");
 exports.app = (0, express_1.default)();
 exports.app.use(routes_1.router);
+exports.app.use(express_1.default.json());
+var options = {
+    allowedHeaders: [
+        'Origin',
+        'Content-Type',
+        'Accept',
+        'authorization',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    preflightContinue: false,
+};
+exports.app.use((0, helmet_1.default)());
+exports.app.use((0, cors_1.default)(options));
+exports.app.options('*', (0, cors_1.default)(options));
--- dist/src/server.js
+++ dist/src/server.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = require("./app");
-app_1.app.listen(3334, function () { return console.log('Servidor rodando na porta 3334: https://localhost:3334'); });
+app_1.app.listen(process.env.PORT || 3334, function () { return console.log('Servidor rodando na porta 3334: https://localhost:3334'); });
